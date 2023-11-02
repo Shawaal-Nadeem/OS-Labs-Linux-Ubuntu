c3b3f858a6d427adc6cee4619b692ffe008c1ea8
Shawaal Nadeem <hafizshawalnadeem@gmail.com>
2023-11-02 15:32:16 +0500

    calculator completed

--- calculator.c
+++ calculator.c
@@ -1,53 +1,55 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void sum(int n1,int n2){
-printf("%d",n1);
-printf("%d",n2);
-int res=n1+n2;
-printf("%d",res);
-printf("\n");
-}
-void sub(int n1,int n2){
-printf("%d",n1-n2);
-printf("\n");
-}
-void mul(int n1,int n2){
-printf("%d",n1*n2);
-printf("\n");
-}
-void div(int n1,int n2){
-printf("%d",n1/n2);
-printf("\n");
+void sum(int n1, int n2) {
+    printf("%d + %d = %d\n", n1, n2, n1 + n2);
 }
 
-int main(){
-int num1,num2;
-char operator;
-printf("Enter First Number : ");
-scanf("%d",&num1);
-printf("Enter Second Number : ");
-scanf("%d",&num2);
-printf("Enter Operator : ");
-scanf("%s",&operator);
+void sub(int n1, int n2) {
+    printf("%d - %d = %d\n", n1, n2, n1 - n2);
+}
 
-if(operator=='+'){
-printf("Adding");
-sum(num1,num2);
+void mul(int n1, int n2) {
+    printf("%d * %d = %d\n", n1, n2, n1 * n2);
 }
-else if(operator=='-'){
-printf("Subtracting");
-sub(num1,num2);
+
+void div(int n1, int n2) {
+    if (n2 != 0) {
+        printf("%d / %d = %d\n", n1, n2, n1 / n2);
+    } else {
+        printf("Division by zero is not allowed.\n");
+    }
 }
-else if(operator=='*'){
-printf("Multiplying");
-mul(num1,num2);
-}
-else if(operator=='/'){
-printf("Dividing");
-div(num1,num2);
-}
-else{
-printf("Wrong Operator");
-}
-return 0;
+
+int main() {
+    int num1;
+    int num2;
+    char operator;
+
+    printf("Enter First Number: ");
+    scanf("%d", &num1);
+    printf("Enter Second Number: ");
+    scanf("%d", &num2);
+    printf("Enter Operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("num1: %d\n", num1); // Add format specifier %d
+    printf("num2: %d\n", num2); // Add format specifier %d
+
+    if (operator == '+') {
+        printf("Adding\n");
+        sum(num1, num2);
+    } else if (operator == '-') {
+        printf("Subtracting\n");
+        sub(num1, num2);
+    } else if (operator == '*') {
+        printf("Multiplying\n");
+        mul(num1, num2);
+    } else if (operator == '/') {
+        printf("Dividing\n");
+        div(num1, num2);
+    } else {
+        printf("Wrong Operator\n");
+    }
+
+    return 0;
 }
